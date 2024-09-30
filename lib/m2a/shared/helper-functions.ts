9f2304c72a94e64b43903e9e2511ed31939946f9
DerSkillBaumApfel <DerSkillBaumApfel@users.noreply.github.com>
2024-09-30 21:12:45 +0200

    renamed configuration properties

--- lib/m2a/shared/helper-functions.ts
+++ lib/m2a/shared/helper-functions.ts
@@ -1,16 +1,16 @@
 import { createError } from "@directus/errors";
 
 /** maps the array elements to objects of type JunctionCollection with a default `foreignKey` field of `“item”` and a default `foreignCollection` field of `“collection”`  */
-export const toJunctionCollectionM2A = (name: string): JunctionCollection => ({
-    name,
-    foreignKey: "item",
-    foreignCollection: "collection",
+export const toJunctionCollectionM2A = (collectionName: string): JunctionCollection => ({
+    collectionName,
+    foreignKeyFieldName: "item",
+    itemDiscriminatorFieldName: "collection",
 });
 
 /** maps the array elements to objects of type AnyCollection with a default `key` field of `“id”`  */
-export const toAnyCollectionM2A = (name: string): AnyCollection => ({
-    name,
-    key: "id",
+export const toAnyCollectionM2A = (collectionName: string): AnyCollection => ({
+    collectionName,
+    primaryKeyFieldName: "id",
 });
 
 export const buildIllegalDeleteError = (watchedItemName: string, relatedItemName: string) => createError(
